e76c4799296bab54bccf48508ceff75726452544
Phil Gebhardt <gebhardt.phil@gmail.com>
2026-09-05 21:28:28 +0000

    parse_spec: split output along derived definitions
    Looks for `DEFINITIONS AUTOMATIC TAGS ::=` to derive output file names
    and file boundaries.
    
    For example: parsing `37355-j30.docx` from `3GPP TS 37.355` (LTE Positioning Protocol)
    
    produces two files: `LPP-PDU-Definitions.asn` and
    `LPP-Broadcast-Definitions.asn` in the supplied target directory.
    
    ```sh
    python3 /git/hampi/examples/specs/parse_spec.py 37355-j30.docx -d specs/
    ```

--- examples/specs/parse_spec.py
+++ examples/specs/parse_spec.py
@@ -6,7 +6,7 @@ import docx
 start_asn = re.compile(r'^-- ASN1START$')
 end_asn = re.compile(r'^-- ASN1STOP$')
 
-def main(spec_file, asn_file):
+def main(spec_file, asn_dir):
 
     doc = docx.Document(spec_file)
     start_printing = False
@@ -19,23 +19,46 @@ def main(spec_file, asn_file):
         if re.match(end_asn, para.text):
             start_printing = False
 
+
     out_lines = [line.replace(chr(0xa0), ' ') for line in out_lines]
+
+    modules = {}
+    last_nonblank = None
+    i_last_nonblank = None
+    module_name = None
+    for i, line in enumerate(out_lines):
+        stripped = line.strip()
+        if stripped == 'DEFINITIONS AUTOMATIC TAGS ::=':
+            if module_name:
+                modules[module_name]['end'] = i_last_nonblank
+            module_name = last_nonblank
+            modules[module_name] = {'start': i_last_nonblank }
+        if stripped:
+            last_nonblank = stripped
+            i_last_nonblank = i
+
+    modules[module_name]['end'] = len(out_lines)
+
     preclude = [
             "--\n",
             f"-- Generated using : {' '.join(sys.argv)}\n",
             "-- DO NOT EDIT BY HAND\n",
             "--\n"]
-    with open(asn_file, 'w') as outfile:
-        outfile.writelines(preclude)
-        outfile.writelines(out_lines)
+    for name, bounds in modules.items():
+        start = bounds['start']
+        end = bounds['end']
+        asn_file = f"{asn_dir}/{name}.asn"
+        with open(asn_file, 'w') as outfile:
+            outfile.writelines(preclude)
+            outfile.writelines(out_lines[start:end])
 
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 4:
-        print("usage: parse_spec.py <spec.docx> -o <out.asn>")
+        print("usage: parse_spec.py <spec.docx> -d <out dir>")
         sys.exit(1)
 
     spec_file = sys.argv[1]
-    asn_file = sys.argv[3]
-    main(spec_file, asn_file)
+    asn_dir = sys.argv[3]
+    main(spec_file, asn_dir)
